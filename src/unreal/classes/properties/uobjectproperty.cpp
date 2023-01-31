0692a106e8a9abf8a89e86e1d791bb1789b715a5
apple1417 <git@apple1417.dev>
2023-01-31 22:53:57 +1300

    allow setting objects to null

--- src/unreal/classes/properties/uobjectproperty.cpp
+++ src/unreal/classes/properties/uobjectproperty.cpp
@@ -11,11 +11,14 @@ PropTraits<UObjectProperty>::Value PropTraits<UObjectProperty>::get(UObjectPrope
 }
 
 void PropTraits<UObjectProperty>::set(UObjectProperty* prop, uintptr_t addr, Value value) {
-    auto prop_cls = prop->read_field(&UObjectProperty::PropertyClass);
-
-    if (!value->is_instance(prop_cls)) {
-        throw std::runtime_error("Object is not instance of " + (std::string)prop_cls->Name);
+    // Ensure the object is of a valid class
+    if (value != nullptr) {
+        auto prop_cls = prop->read_field(&UObjectProperty::PropertyClass);
+        if (!value->is_instance(prop_cls)) {
+            throw std::runtime_error("Object is not instance of " + (std::string)prop_cls->Name);
+        }
     }
+
     *reinterpret_cast<Value*>(addr) = value;
 }
 
