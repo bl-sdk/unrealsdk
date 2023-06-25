99f94f1a95f8c29f0a70b878519f23219430cc6f
apple1417 <git@apple1417.dev>
2023-06-25 17:28:04 +1200

    track git hash in version

--- src/unrealsdk/version.h
+++ src/unrealsdk/version.h
@@ -9,17 +9,22 @@ inline constexpr auto VERSION_MAJOR = 1;
 inline constexpr auto VERSION_MINOR = 0;
 inline constexpr auto VERSION_PATCH = 0;
 
-extern const std::string VERSION_STR;
-
 #ifdef UNREALSDK_SHARED
 /**
- * @brief Gets the version of the sdk.
+ * @brief Gets the version number of the sdk.
  * @note Packs as `(major & 0xFF) << 16 | (minor & 0xFF) << 8 | (patch & 0xFF)`.
  *
  * @return The packed version number the sdk was compiled with.
  */
 UNREALSDK_CAPI uint32_t get_version(void);
 #endif
+
+/**
+ * @brief Get the sdk version as a printable string.
+ *
+ * @return The sdk version string.
+ */
+const std::string& get_version_string(void);
 
 }  // namespace unrealsdk
 
