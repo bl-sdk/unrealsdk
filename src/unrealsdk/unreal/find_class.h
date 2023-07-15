ee7bd662eb2a47a6a585a8ab97553bdb69c86c22
apple1417 <git@apple1417.dev>
2023-07-15 14:07:42 +1200

    templated find class

--- src/unrealsdk/unreal/find_class.h
+++ src/unrealsdk/unreal/find_class.h
@@ -2,6 +2,7 @@
 #define UNREALSDK_UNREAL_FIND_CLASS_H
 
 #include "unrealsdk/pch.h"
+#include "unrealsdk/unreal/class_name.h"
 
 namespace unrealsdk::unreal {
 
@@ -14,10 +15,14 @@ class UClass;
  *       returns an undefined instance. Calling with a string performs a fully qualified lookup,
  *       which is less efficient, but properly handles this case.
  *
+ * @tparam T The type to get the class object of.
  * @param name The class name.
- * @param name_size The size of the class name string.
  * @return The class object.
  */
+template <typename T>
+[[nodiscard]] UClass* find_class(void) {
+    return find_class(cls_fname<T>());
+}
 [[nodiscard]] UClass* find_class(const FName& name);
 [[nodiscard]] UClass* find_class(const std::wstring& name);
 
